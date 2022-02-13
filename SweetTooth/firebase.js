47f33c73cd249765929abaa0639a5837c7e248ce
Sid Ghimire <siddharthaghimire@gmail.com>
2022-02-13 13:04:36 +0545

    Delete SweetTooth directory

--- SweetTooth/firebase.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyDx0MBfZ5EGqvWyU-x4fddTWZgqUkq8yvg",
-    authDomain: "sweettooth-sid.firebaseapp.com",
-    projectId: "sweettooth-sid",
-    storageBucket: "sweettooth-sid.appspot.com",
-    messagingSenderId: "695771666125",
-    appId: "1:695771666125:web:01921d8e84662f2c347b84"
-  };
